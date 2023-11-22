1ee782efbe181d9e2a918c269cec0c9aae23c187
sbk03 <sbk0301@naver.com>
2023-11-22 14:50:48 +0900

    이미지 업로드 및 개발 해야할 클래스 생성

---
+++ GradeHunter/Panel_t.java
@@ -0,0 +1,39 @@
+package GradeHunter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ * 엔딩크레딧장면에서 랭크패널로 이동하는 역할을 담당하는 클래스입니다.
+ * @author 서보경
+ */
+public class Panel_t{
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            Toolkit kit = Toolkit.getDefaultToolkit();
+            JFrame frame = new JFrame("Grade Hunter");
+            frame.setSize(1080, 720);
+
+            Dimension screenSize = kit.getScreenSize();
+
+            Image img = kit.getImage("images\\Gradcap.png");
+            frame.setIconImage(img);
+
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            // 첫 번째 패널
+            EndingPanel endingPanel = new EndingPanel(frame);
+            frame.add(endingPanel);
+
+            // 두 번째 패널
+            EndCreditsPanel endCreditsPanel = new EndCreditsPanel(frame);
+
+            frame.setVisible(true);
+        });
+    }
+}
