507189c14ed62b3f569f2e8d58d0dcf1ca8ac5c2
sbk03 <sbk0301@naver.com>
2023-11-29 12:03:44 +0900

    퀴즈 셀렉트 패널 로직 변경 중

--- GradeHunter/ClearPanel.java
+++ GradeHunter/ClearPanel.java
@@ -13,6 +13,7 @@ import java.io.IOException;
  */
 
 
+/*
 public class ClearPanel extends JPanel {
 
     public static JFrame frame;
@@ -43,5 +44,55 @@ public class ClearPanel extends JPanel {
         if (backgroundImage != null) {
             g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
         }
+    }
+}
+*/
+
+
+public class ClearPanel extends JPanel {
+    private Image backgroundImage;
+
+    public ClearPanel(MainPanel mainPanel) {
+        setLayout(null); // 널 레이아웃 사용
+        backgroundImage = new ImageIcon("GradeHunter/images/bg_gp_1.png").getImage();
+
+        ImageIcon leftIcon = new ImageIcon("GradeHunter/images/key_left.png");
+        ImageIcon rightIcon = new ImageIcon("GradeHunter/images/key_right.png");
+        JButton leftButton = new JButton(leftIcon);
+        JButton rightButton = new JButton(rightIcon);
+
+        leftButton.setBounds(60, 550, leftIcon.getIconWidth(), leftIcon.getIconHeight());
+        rightButton.setBounds(955, 550, rightIcon.getIconWidth(), rightIcon.getIconHeight());
+
+        leftButton.setContentAreaFilled(false);
+        leftButton.setBorderPainted(false);
+        rightButton.setContentAreaFilled(false);
+        rightButton.setBorderPainted(false);
+
+        // 오른쪽 버튼 이벤트 리스너
+        rightButton.addActionListener(e -> {
+            QuizSelectPanel quizSelectPanel = new QuizSelectPanel(mainPanel);
+            mainPanel.switchPanel(quizSelectPanel);
+        });
+
+        // 왼쪽 버튼 이벤트 리스너
+        leftButton.addActionListener(e -> {
+
+            // MainPanel의 값 초기화 후 전환
+            mainPanel.resetValues();
+
+            // MainPanel의 메인 콘텐츠 패널로 돌아가기
+            mainPanel.switchPanel(mainPanel.getMainContentPanel());
+        });
+
+
+        add(leftButton);
+        add(rightButton);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
     }
 }
