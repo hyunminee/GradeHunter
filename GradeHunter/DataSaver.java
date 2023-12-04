1e20d8872b2a91e58ce45e5d70784783aaff566c
sbk03 <sbk0301@naver.com>
2023-12-04 17:08:06 +0900

    최최종

--- GradeHunter/DataSaver.java
+++ GradeHunter/DataSaver.java
@@ -27,29 +27,28 @@ public class DataSaver {
         try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("data.txt", true), StandardCharsets.UTF_8))) {
             writer.write(record);  // 파일에 기록을 작성합니다.
             writer.newLine();  // 다음 기록을 위해 새 줄을 추가합니다.
-            } catch(IOException e){
-                e.printStackTrace(); // IOException이 발생하면 오류를 출력합니다.
-            }
+        } catch(IOException e){
+            e.printStackTrace(); // IOException이 발생하면 오류를 출력합니다.
         }
-
-        /**
-         * 정수로 된 과목 코드를 문자열로 변환하는 메소드
-         * <p> 각 과목 코드에 해당하는 과목 이름을 문자열로 반환합니다. </p>
-         * @param subject 과목 코드
-         * @return 해당 과목 코드에 따른 과목명
-         */
-        public String convertSubjectToString (int subject ){
-            // 과목 코드에 따라 과목 이름을 매핑하는 switch 문입니다.
-            return switch (subject) {
-                case 1 -> "국어";  // 1번 경우: 국어
-                case 2 -> "수학";  // 2번 경우: 수학
-                case 3 -> "영어";  // 3번 경우: 영어
-                case 4 -> "지리";  // 4번 경우: 지리
-                case 5 -> "역사";  // 5번 경우: 역사
-                case 6 -> "과학";  // 6번 경우: 과학
-                default -> "알 수 없음"; // 기본 경우: 알 수 없음
-            };
-        }
-
     }
+
+    /**
+     * 정수로 된 과목 코드를 문자열로 변환하는 메소드
+     * <p> 각 과목 코드에 해당하는 과목 이름을 문자열로 반환합니다. </p>
+     * @param subject 과목 코드
+     * @return 해당 과목 코드에 따른 과목명
+     */
+    public String convertSubjectToString (int subject ){
+        // 과목 코드에 따라 과목 이름을 매핑하는 switch 문입니다.
+        return switch (subject) {
+            case 1 -> "국어";  // 1번 경우: 국어
+            case 2 -> "수학";  // 2번 경우: 수학
+            case 3 -> "영어";  // 3번 경우: 영어
+            case 4 -> "지리";  // 4번 경우: 지리
+            case 5 -> "역사";  // 5번 경우: 역사
+            case 6 -> "과학";  // 6번 경우: 과학
+            default -> "알 수 없음"; // 기본 경우: 알 수 없음
+        };
+    }
+
 }
